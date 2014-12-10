fc3091ac88afcd1382a4e9c2430ecfe99761d318
cespirit <ckespir@gmail.com>
2014-12-09 20:00:22 -0800

    Initial commit working fizzbuzz

---
+++ app.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	$("button").click(runFizzBuzz);	
+	$("#number").keyup(function(e){
+		if(e.keyCode === 13) {
+			runFizzBuzz();
+		}
+	});
+});
+
+function runFizzBuzz(){
+	$("#results").empty();
+	
+	var num = $("#number").val();	
+	
+	if(isValidNumber(num)) {
+		fizzBuzz(num);
+	}
+	else {
+		alert("Please enter a non-decimal number greater than zero");
+	}
+	
+	$("#number").val("");
+}
+
+function isValidNumber(num) {	
+	if(num.indexOf(".") != -1){
+		return false;
+	}
+	
+	num = +$.trim(num);
+	
+	if(!num || num <= 0) {		
+		return false;		
+	}	
+		
+	return true;
+}
+
+function fizzBuzz(num) {
+	var printStr;
+	$("#results").append("<h3>Fizz buzz up to " + num + "</h3>");
+	
+	for(var i = 1; i <= num; i++) {
+		printStr = "";
+		if(i % 3 === 0) {
+			printStr += "fizz";
+		}
+		if(i % 5 === 0) {
+			printStr += "buzz";
+		}		
+		if(!printStr) {
+			printStr = i;
+		}
+		$("#results").append("<p>" + printStr + "</p>");
+	}
+}
+
+/*
+Test inputs
+""
+" "  
+23.8  
+-4		
+-5.3  
+"Not a number"
+"43 Not a number"
+*/
